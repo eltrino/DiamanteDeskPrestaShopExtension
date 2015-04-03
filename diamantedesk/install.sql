aa2de58c7ed08511f546aefbdbf771e328f50c32
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-04-03 12:59:34 +0300

    upadte upgrade script

--- diamantedesk/install.sql
+++ diamantedesk/install.sql
@@ -14,11 +14,11 @@
 
 CREATE TABLE IF NOT EXISTS `PREFIX_diamantedesk_order_relation` (
   `relation_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ticket_id`   INT(10) UNSIGNED NOT NULL,
+  `ticket_key`   VARCHAR(200) NOT NULL,
   `order_id`    INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`relation_id`),
-  KEY `id_product` (`ticket_id`),
-  KEY `id_customer` (`order_id`)
+  KEY `ticket_key` (`ticket_key`),
+  KEY `order_id` (`order_id`)
 )
   ENGINE =ENGINE_TYPE
   DEFAULT CHARSET =utf8;
